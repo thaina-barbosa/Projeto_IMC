f17e43452dce97739dd928bca77b5a19ea73ff04
thaina-barbosa <thaina.mbarbosaa@gmail.com>
2022-07-25 21:24:36 -0300

    Apagando comentario

--- IMC/Program.cs
+++ IMC/Program.cs
@@ -18,13 +18,7 @@ namespace IMC
             Console.WriteLine("Digite a sua Altura: ");
             float altura = (float) Convert.ToDouble(Console.ReadLine());
           
-            //REGRA IMC
-            //Menos do que 18,5   Abaixo do peso
-            //Entre 18,5 e 24,9   Peso normal
-            //Entre 25 e 29,9 Sobrepeso
-            //Entre 30 e 34,9 Obesidade grau 1
-            //Entre 35 e 39,9 Obesidade grau 2
-            //Mais do que 40  Obesidade grau 3
+            
 
             float result = peso / (altura * altura);
 
